67f281b865cbd37c28640fa6177d884788be176f
adwitafernanda <adwitafernanda4@gmail.com>
2023-06-21 14:46:47 +0700

    menunjuk ke sebuah nama file

--- IoFile/IoFile.cpp
+++ IoFile/IoFile.cpp
@@ -16,7 +16,7 @@ int main() {
 	//unlimited loop untuk menulis
 	while (true) {
 		cout << "- ";
-		//mendapatkan setiap karakter dalam satu baris
+		//mendapatkan setiap karakter dalam satu baris 
 		getline(cin, baris);
 		//loop akan berhenti jika anda memasukkan karakter q
 	}
